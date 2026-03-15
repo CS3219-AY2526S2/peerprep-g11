a0f59c180009c113c76cf9dc678ab9211818c13b
Ken-Lai <weizhong9201@gmail.com>
2026-03-16 03:47:01 +0800

    Merge add and update methods

--- question-service/main.py
+++ question-service/main.py
@@ -1,7 +1,8 @@
 import os
+from datetime import datetime, timezone
 from dotenv import load_dotenv
 from fastapi import FastAPI
-from pymongo import AsyncMongoClient
+from pymongo import AsyncMongoClient, ReturnDocument
 from schema import QuestionSchema, DeleteSchema
 
 load_dotenv()
@@ -26,10 +27,27 @@ async def get_question(question_title: str):
     question = await collection.find_one(filter)
     return question
 
-@app.post('/questions/add', status_code=201)
+@app.post('/questions/upsert', status_code=201)
 async def add_question(question: QuestionSchema):
-    result = await collection.insert_one(question.model_dump())
-    return {"acknowledged": result.acknowledged}
+    data = question.model_dump()
+    title = data['title']
+    now = datetime.now(timezone.utc).isoformat()
+
+    filter = {'title': title}
+    set_fields = {key: value for key, value in data.items() if key != 'title'}
+    set_fields['updated_at'] = now
+    update = {
+        '$set': set_fields,
+        '$setOnInsert': {'created_at': now, 'title': title}
+    }
+
+    result = await collection.find_one_and_update(filter, update, upsert=True, return_document=ReturnDocument.AFTER)
+    is_inserted = result['created_at'] == now
+    
+    if is_inserted:
+        return {'message': "Question added.", 'title': title}
+    
+    return {'message': "Question updated.", 'title': title}
 
 @app.delete('/questions/delete')
 async def delete_question(question: DeleteSchema):
