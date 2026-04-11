6c1ef69c9e942c6cd8525f716fea1dbc07017a57
Low Xi Si <e0960422@u.nus.edu>
2026-04-12 04:40:25 +0800

    Add LuaScriptConfig to inject lua scripts as Spring beans

---
+++ matching-service/src/main/java/peerprep/matching/config/LuaScriptConfig.java
@@ -0,0 +1,49 @@
+package peerprep.matching.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.scripting.support.ResourceScriptSource;
+
+import java.util.List;
+
+@Configuration
+public class LuaScriptConfig {
+
+    @Bean
+    public DefaultRedisScript<List> tryStrictMatchScript() {
+        DefaultRedisScript<List> script = new DefaultRedisScript<>();
+        script.setScriptSource(new ResourceScriptSource(
+                new ClassPathResource("lua/try_strict_match.lua")));
+        script.setResultType(List.class);
+        return script;
+    }
+
+    @Bean
+    public DefaultRedisScript<List> tryRelaxedMatchScript() {
+        DefaultRedisScript<List> script = new DefaultRedisScript<>();
+        script.setScriptSource(new ResourceScriptSource(
+                new ClassPathResource("lua/try_relaxed_match.lua")));
+        script.setResultType(List.class);
+        return script;
+    }
+
+    @Bean
+    public DefaultRedisScript<Long> finalizeMatchScript() {
+        DefaultRedisScript<Long> script = new DefaultRedisScript<>();
+        script.setScriptSource(new ResourceScriptSource(
+                new ClassPathResource("lua/finalize_match.lua")));
+        script.setResultType(Long.class);
+        return script;
+    }
+
+    @Bean
+    public DefaultRedisScript<Long> addToPendingFinalizationScript() {
+        DefaultRedisScript<Long> script = new DefaultRedisScript<>();
+        script.setScriptSource(new ResourceScriptSource(
+                new ClassPathResource("lua/add_to_pending.lua")));
+        script.setResultType(Long.class);
+        return script;
+    }
+}
